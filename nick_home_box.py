18d1dc36a8349228af3b6070c234d1281948ff0f
Nick Booker <NMBooker@gmail.com>
2022-10-13 17:37:17 +0100

    Move heredoc function into module [REF]
    So I can start the declarations sooner

--- nick_home_box.py
+++ nick_home_box.py
@@ -5,45 +5,14 @@ from socket import gethostname
 import sys
 from typing import AbstractSet, Iterable, Set, TypeAlias
 
+from heredocs import heredoc
+
 Hostname: TypeAlias = str
 FlatpakName: TypeAlias = str
 GnomeExtension: TypeAlias = str
 PackageName: TypeAlias = str
 RepoTag: TypeAlias = str
 Role: TypeAlias = str
-
-
-def heredoc(astring: str) -> str:
-    r"""Unindent an indented multi-line string.
-
-    This is to allow multi-line strings to be indented similar to
-    Python code without the indentation making it into the final result.
-    Line endings are preserved as written.
-
-    >>> indented_multiline('''
-        hello:
-         world
-        ''')
-    'hello:\n world\n'
-
-    It's not possible to directly build a string whose last line
-    does not end with a new line, but you can use .rstrip('\n')
-    on the result if that's what you want.  I anticipate you'll be building
-    a text file using this so a trailing newline is nearly always what
-    you want.
-    """
-    lines = astring.splitlines(keepends=True)
-    first_line = lines[0]
-    content_lines = lines[1:-1]
-    last_line = lines[-1]
-    assert first_line.isspace(), 'first line must be all whitespace'
-    assert last_line.isspace(), 'last line must be all whitespace'
-    indentation_template = last_line.rstrip('\n')
-    assert all(l.startswith(indentation_template) for l in content_lines), \
-        'all content lines must begin with same whitespace as last one'
-    unindented_lines = \
-        [l.removeprefix(indentation_template) for l in content_lines]
-    return ''.join(unindented_lines)
 
 
 def yumrepo(name, filestem, baseurl, gpg_key_url):
